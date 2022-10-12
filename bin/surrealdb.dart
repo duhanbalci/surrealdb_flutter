3870fc1d4e3b604b2c4979f39926ec2fcddf9526
Duhan BALCI <duhanbalci@msn.com>
2022-10-12 19:54:03 +0300

    test

--- bin/surrealdb.dart
+++ bin/surrealdb.dart
@@ -6,13 +6,33 @@ void main(List<String> args) async {
 
   client.connect();
   await client.wait();
-  await client.use('ns', 'db');
+  await client.use('test', 'test');
   await client.signin('root', 'root');
 
-  final delete = await client.query('DELETE user:5');
+  await client.create('person', TestModel(false, 'title'));
 
-  print(delete);
-  client.close();
+  var person = await client.create('person', {
+    'title': 'Founder & CEO',
+    'name': {
+      'first': 'Tobie',
+      'last': 'Morgan Hitchcock',
+    },
+    'marketing': false,
+  });
+  print(person);
+
+  List<Map<String, Object?>> persons = await client.select('person');
+
+  final groupBy = await client.query(
+    'SELECT marketing, count() FROM type::table(\$tb) GROUP BY marketing',
+    {
+      'tb': 'person',
+    },
+  );
+
+  print(groupBy);
+
+  print(persons.length);
 }
 
 class AMODEL {
--- test/surrealdb_test.dart
+++ test/surrealdb_test.dart
@@ -1,7 +1,87 @@
 import 'package:test/test.dart';
 
-// import 'package:surrealdb/surrealdb.dart';
+import 'package:surrealdb/surrealdb.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('should connect & disconnect', () async {
+    final client = SurrealDB('ws://localhost:8000/rpc');
+    client.connect();
+    await client.wait();
+    client.close();
+    await Future.delayed(Duration(seconds: 1));
+  });
+
+  test('should connect, use, signin, close', () async {
+    final client = SurrealDB('ws://localhost:8000/rpc');
+    client.connect();
+    await client.wait();
+    await client.use('ns', 'db');
+    await client.signin('root', 'root');
+    client.close();
+    await Future.delayed(Duration(seconds: 1));
+  });
+
+  test('should create', () async {
+    final client = SurrealDB('ws://localhost:8000/rpc');
+    client.connect();
+    await client.wait();
+    await client.use('ns', 'db');
+    await client.signin('root', 'root');
+    var data = {
+      'title': 'Founder & CEO',
+      'name': {
+        'first': 'Tobie',
+        'last': 'Morgan Hitchcock',
+      },
+      'marketing': false,
+    };
+    var res = await client.create('person', data);
+    var person = (res as List)[0] as Map<String, dynamic>;
+
+    expect(person['id'], isNotNull);
+    expect(person['title'], data['title']);
+    expect(person['name']['first'],
+        (data['name'] as Map<String, dynamic>)['first']);
+    expect(
+        person['name']['last'], (data['name'] as Map<String, dynamic>)['last']);
+    expect(person['marketing'], data['marketing']);
+  });
+
+  test('should delete,select', () async {
+    final client = SurrealDB('ws://localhost:8000/rpc');
+    client.connect();
+    await client.wait();
+    await client.use('ns', 'db');
+    await client.signin('root', 'root');
+    var data = {
+      'title': 'Founder & CEO',
+      'name': {
+        'first': 'Tobie',
+        'last': 'Morgan Hitchcock',
+      },
+      'marketing': false,
+    };
+    await client.delete('person');
+    var res = await client.create('person', data);
+    var person = (res as List)[0] as Map<String, dynamic>;
+
+    expect(person['id'], isNotNull);
+    expect(person['title'], data['title']);
+    expect(person['name']['first'],
+        (data['name'] as Map<String, dynamic>)['first']);
+    expect(
+        person['name']['last'], (data['name'] as Map<String, dynamic>)['last']);
+    expect(person['marketing'], data['marketing']);
+
+    var persons = await client.select('person');
+    expect(persons.length, 1);
+    person = persons[0] as Map<String, dynamic>;
+    expect(person['id'], isNotNull);
+    expect(person['title'], data['title']);
+    expect(person['name']['first'],
+        (data['name'] as Map<String, dynamic>)['first']);
+    expect(
+        person['name']['last'], (data['name'] as Map<String, dynamic>)['last']);
+    expect(person['marketing'], data['marketing']);
+  });
 }
