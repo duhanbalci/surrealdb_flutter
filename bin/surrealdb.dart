1f9b52fcffb869bfdef62570e2aeef40cc5604a9
duhanbalci <duhan.balci@rempeople.com>
2024-09-22 21:01:58 +0300

    fix dart analyze warnings

--- bin/surrealdb.dart
+++ bin/surrealdb.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: avoid_print
 
-import 'package:surrealdb/src/common/types.dart';
 import 'package:surrealdb/surrealdb.dart';
 
 void main(List<String> args) async {
@@ -11,45 +10,7 @@ void main(List<String> args) async {
   // Use a specific namespace and database
   await db.use('namespace', 'database');
 
-//   await db.signin(user: 'root', pass: 'root');
-
-//   await db.query(r'''
-// DEFINE ACCESS users ON DATABASE TYPE RECORD
-// 	SIGNUP ( CREATE user SET email = $email, pass = crypto::argon2::generate($pass) )
-// 	SIGNIN ( SELECT * FROM user WHERE email = $email AND crypto::argon2::compare(pass, $pass) )
-// 	DURATION FOR TOKEN 15m, FOR SESSION 12h;
-// ''');
-
-  final token = await db.signin(
-    user: 'root',
-    pass: 'root',
-    // namespace: 'namespace',
-    // database: 'database',
-    // access: 'users',
-  );
-  // print('User signed up with token: $token');
-
-  // await db.authenticate(token);
-
-  // Fetch version information
-  // final version = await db.query(r'SELECT * FROM $session');
-  // print('SurrealDB version: $version');
-
-  final liveQuery =
-      await db.liveQuery('LIVE SELECT * FROM posts WHERE active = true');
-  liveQuery.stream.listen((event) {
-    print('Action: "${event.action}", result: ${event.result}');
-  });
-
-  await db.create('posts', {
-    'title': 'My first post',
-    'content': 'Hello, SurrealDB!',
-  });
-  await db.create('posts', {
-    'title': 'My second post',
-    'content': 'Hello, SurrealDB!',
-    'active': true
-  });
+  await db.signin(user: 'root', pass: 'root');
 }
 
 class AMODEL {
